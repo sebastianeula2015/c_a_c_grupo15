28390cedc35fd9d6116229e43707872d4415f2c2
sebastianeula2015 <sebastianeula@gmail.com>
2024-06-04 19:41:41 -0300

    Nuevos modelos

--- web/models.py
+++ web/models.py
@@ -8,41 +8,47 @@ provincias = [
     [3, "La Rioja"],
     [4, "Neuquen"],
 ]
-class Cliente(models.Model):
+
+
+class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    dni = models.IntegerField()
-    email = models.CharField(max_length=60, default='')
-    telefono = models.CharField(max_length=10, default='No')
-    provincia = models.CharField(max_length=50)
-    direccion = models.CharField(max_length=100)
-
-
-    def __str__(self):
-        return self.nombre + ', ' + self.apellido
-
-class Vendedor(models.Model):
-    nombre = models.CharField(max_length=255)
-    apellido = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     telefono = models.CharField(max_length=20)
 
+    class Meta:
+        abstract = True
+
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
+
+
+class Cliente(Persona):
+    dni = models.IntegerField()
+    provincia = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+
+class Proveedor(Persona):
+    cuit = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+
+
+class Vendedor(Persona):
+    nro_vendedor = models.IntegerField(default=0)
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     proveedor = models.ForeignKey('Proveedor', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.nombre
-
-class Proveedor(models.Model):
-    nombre = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    telefono = models.CharField(max_length=20)
 
     def __str__(self):
         return self.nombre
@@ -56,6 +62,7 @@ class Venta(models.Model):
 
     def __str__(self):
         return f"Venta del {self.fecha} para {self.cliente}"
+
 
 class DetalleVenta(models.Model):
     producto = models.ForeignKey('Producto', on_delete=models.CASCADE)
